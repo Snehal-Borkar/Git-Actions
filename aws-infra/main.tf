e24515a35f86fc39a6a479e3a12866b357482bcf
Snehal-Borkar <borkarsnehal60@gmail.com>
2026-07-19 00:43:40 +0530

    add tera change

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -1,29 +1,40 @@
 resource "aws_iam_role" "lambda_role" {
 
-  name = var.role_name
+  name = "hello-lambda-role"
 
   assume_role_policy = jsonencode({
+
     Version = "2012-10-17"
 
-    Statement = [{
-      Action = "sts:AssumeRole"
+    Statement = [
 
-      Effect = "Allow"
+      {
 
-      Principal = {
-        Service = "lambda.amazonaws.com"
+        Action = "sts:AssumeRole"
+
+        Effect = "Allow"
+
+        Principal = {
+
+          Service = "lambda.amazonaws.com"
+
+        }
+
       }
-    }]
+
+    ]
+
   })
+
 }
 
 resource "aws_iam_role_policy_attachment" "basic" {
 
-  role       = aws_iam_role.lambda_role.name
+  role = aws_iam_role.lambda_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
 
+}
 
 resource "aws_lambda_function" "lambda" {
 
@@ -35,7 +46,8 @@ resource "aws_lambda_function" "lambda" {
 
   handler = "app.lambda_handler"
 
-  filename = "../src/lambda.zip"
+  s3_bucket = var.lambda_bucket
 
-  source_code_hash = filebase64sha256("../src/lambda.zip")
+  s3_key = var.lambda_key
+
 }
--- aws-infra/variables.tf
+++ aws-infra/variables.tf
@@ -1,7 +1,23 @@
-variable "lambda_name" {
-  default = "demo-lambda"
+variable "aws_region" {
+
+  default = "ap-south-1"
+
 }
 
-variable "role_name" {
-  default = "lambda-execution-role"
+variable "lambda_name" {
+
+  default = "hello-lambda"
+
+}
+
+variable "lambda_bucket" {
+
+  default = "lambda-artifacts-demo-dev"
+
+}
+
+variable "lambda_key" {
+
+  default = "lambda-packages/hello-lambda-dev.zip"
+
 }
